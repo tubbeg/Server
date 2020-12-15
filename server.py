2817e4cd9acdb817bc20c54d6929f7b6e1eb6e88
Tobias Gärdebro <tubbeg@gmail.com>
2020-12-15 21:28:36 +0100

    Added signal handler to server class.

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from environ import Environment
+import signal
+import sys
 
 
 class WSGI_Server():
     def __init__(self):
-        raise NotImplementedError()
+        pass
 
     def start_response(self, status, response_headers, exc_info=None):
         raise NotImplementedError()
@@ -15,8 +17,8 @@ class WSGI_Server():
 class Server(WSGI_Server):
     def __init__(self):
         super().__init__()
-        self.__environment = Environment()
-        raise NotImplementedError()
+        #self.__environment = Environment()
+        pass
 
     def process_request(self, client_connection):
         raise NotImplementedError()
@@ -29,3 +31,10 @@ class Server(WSGI_Server):
 
     def serve_forever(self):
         raise NotImplementedError()
+
+    def set_signal_handler(self):
+        signal.signal(signal.SIGINT, self.__signal_handler)
+
+    def __signal_handler(self, sig, frame):
+        print("SIGINT detected", file=sys.stderr)
+        sys.exit(0)
--- test_server.py
+++ test_server.py
@@ -1,6 +1,12 @@
-
+from server import Server
+import time
 
 def make_server():
+    server = Server()
+    server.set_signal_handler()
+    for _ in range(0, 100):
+        print(10)
+        time.sleep(10)
     raise NotImplementedError()
 
 
